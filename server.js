a023e645b6fca2e0ef97d821178357a1b9798698
Katherine Perez <perezkh@gmail.com>
2020-07-05 21:35:13 -0700

    minimal game functionality

--- server.js
+++ server.js
@@ -2,17 +2,76 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 4001;
+const fetch = require("node-fetch");
 
-var clients = [];
+const DEFAULT_ROOM = "abcd";
+var rooms = [{id: DEFAULT_ROOM, clients: []}];
+var clients = rooms.filter(room => room.id  === DEFAULT_ROOM)[0].clients;
+let room = DEFAULT_ROOM;
 
-io.on("connection", socket => {
+const usernameApi = "http://randomuser.me/api";
+var timer = 0;
+var timerHandle;
+
+async function getRandomUsername() {
+  await fetch(usernameApi)
+    .then(res => res.json())
+    .then(data => user = {
+      name: data.results[0].name.first,
+      uuid: data.results[0].login.uuid,
+      clicks: 0,
+    });
+  return user;
+}
+
+function getWinner() {
+  let players = clients.sort((a, b) => a.clicks < b.clicks ? 1 : -1);
+  let winners = [];
+  let topClicks = 0;
+  for (player of players) {
+    if (player.clicks >= topClicks) {
+      winners.push(player);
+      topClicks = player.clicks;
+    }
+  }
+  if (winners.length > 1) {
+    console.log("there was a tie");
+  }
+  io.to(room).emit("endGame", winners);
+}
+
+function timerHandler() {
+  timer = timer - 1;
+  if (timer === 0) {
+    clearInterval(timerHandle);
+    let winner = getWinner();
+  }
+}
+
+io.on("connection", async socket => {
   console.log("Client [" + socket.id + "] connected");
-  clients.push({id: socket.id, username: socket.id});
-  io.sockets.emit("updateClients", clients);
+  let user = await getRandomUsername();
+  user['socket'] = socket.id;
+  socket.emit("userDetails", user);
+  socket.join(room);
+  clients.push(user);
+  io.to(room).emit("updateClients", clients);
+  socket.on("startGame", res => {
+    console.log("Starting game");
+    timer = 5;
+    timerHandle = setInterval(timerHandler,1000);
+    io.to(room).emit("startGame", {timer: timer});
+  });
+  socket.on("updateClicks", res => {
+    let cIdx = clients.findIndex(element =>
+      element.uuid === res.uuid);
+    clients[cIdx] = {...clients[cIdx], clicks: res.clicks};
+    io.to(room).emit("updateClients", clients);
+  });
   socket.on("disconnect", res => {
     console.log("Client [" + socket.id + "] disconnected");
-    const connectedClients = clients.filter((client) => client.id !== socket.id);
-    io.sockets.emit("updateClients", connectedClients);
+    clients = clients.filter((client) => client.socket !== socket.id);
+    io.sockets.emit("updateClients", clients);
   });
 });
 
--- src/App.js
+++ src/App.js
@@ -7,44 +7,124 @@ class App extends Component {
     super();
     this.state = {
       endpoint: 'http://localhost:4001',
-      id: "",
+      uuid: "",
       clients: [],
+      clicks: 0,
+      timer: 0,
+      socket: null,
+      winners: [],
+      sGameOver: false,
+      sGameStart: false,
     };
+  }
+
+  timerHandler = () => {
+    if (this.state.timer === 0) {
+      clearInterval(this.state.timerId);
+      this.state.socket.emit("endGame", "yup it's over");
+    } else {
+      this.setState(prevState => {
+        return {
+          timer: prevState.timer - 1,
+        }
+      });
+    }
   }
 
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-    socket.on("connect", () => {
-      socket.emit("addClient", socket.id);
+    this.setState({
+      socket: socket,
     });
-    socket.on("updateClients", res => {
+    socket.on("userDetails", res => {
       this.setState({
-        clients: res,
+        uuid: res.uuid,
+        name: res.name,
+      });
+    });
+    socket.on("updateClients", clients => {
+      this.setState({
+        clients: clients,
+      });
+    });
+    socket.on("startGame", res => {
+      this.setState({
+        timer: res.timer,
+        timerId: setInterval(this.timerHandler, 1000),
+        sGameStart: true,
+      });
+    });
+    socket.on("endGame", res => {
+      this.setState({
+        winners: res,
+        sGameOver: true,
+        sGameStart: false,
       });
     });
   }
 
   render() {
-    const { id } = this.state;
-
     const clients = this.state.clients.map(client => {
       return (
-        <li key={client.id}>
-          {client.username}
-        </li>
+        <div className="IndividualScore" key={client.uuid}>
+          <div className="PlayerName">
+            {client.name}
+          </div>
+          <div>
+            {client.clicks}
+          </div>
+        </div>
       );
     });
+    const { uuid, name, timer, clicks } = this.state;
+    let winner = null;
+    if (this.state.winners.length > 0) {
+      winner = this.state.winners[0].name;
+    }
     return (
       <div className="App">
-        <div>Hello {this.state.username}</div>
-        <div>
-          Players
-          <ul>
-            {clients}
-          </ul>
+        <div className="Title">
+          Hayai
         </div>
-        <button>Click</button>
+        <div>
+          <div className="Game">
+            <div>UUID: {uuid}</div>
+            <div>Player name: {name}</div>
+            {
+              this.state.sGameOver &&
+              <div>Winner: {winner}</div>
+            }
+            <div className="Players-Container">
+              <div className="Players">
+                {clients} 
+              </div>
+            </div>
+            {
+              this.state.sGameStart &&
+              <div>
+                <div className="Timer">
+                  Time left: {timer}
+                </div>
+                <button onClick={() => { 
+                  let incClicks = clicks+1;
+                  this.setState({
+                    clicks: incClicks,
+                  });
+                  this.state.socket.emit("updateClicks", {uuid: this.state.uuid, clicks: incClicks});
+                }}>Click</button>
+              </div>
+            }
+            <div className="Game-Controls">
+              <button className="Start"
+                onClick={() => { this.state.socket.emit("startGame");}}
+              >
+                Start Game
+              </button>
+              <button className="Reset">Reset Game</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
